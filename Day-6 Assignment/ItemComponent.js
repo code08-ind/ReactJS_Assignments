608b2ec6098d84cd61e4f9d3b81fd96c4a4fbd89
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2024-03-05 15:57:37 +0530

    Update ItemComponent.js

--- Day-6 Assignment/ItemComponent.js
+++ Day-6 Assignment/ItemComponent.js
@@ -1,19 +1,13 @@
 import { connect } from "react-redux";
 import { addItem, removeItem } from "../actions/actions";
 
-const ItemComponent = ({ itemCount, addItem, removeItem }) => {
+const ItemComponent = ({ addItem, removeItem }) => {
     return (
         <div>
             <button onClick={addItem}>Add Item</button>
             <button onClick={removeItem}>Remove Item</button>
-            <p>Item Count: {itemCount}</p>
-            <p style={{color:"red"}}>{itemCount===0 && <span>Item Count Cannot Be Less Than 0.</span>}</p>
         </div>
     );
 };
 
-const mapStateToProps = (state) => ({
-    itemCount: state.itemCount
-});
-
-export default connect(mapStateToProps, { addItem, removeItem })(ItemComponent);
+export default connect(null, { addItem, removeItem })(ItemComponent);
